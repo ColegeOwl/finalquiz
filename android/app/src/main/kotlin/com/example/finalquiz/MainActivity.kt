7c6a62d13e57c0c98260e6c9df90b16092c7274f
Themistokli Dajko <themidajko02@gmail.com>
2025-03-12 21:06:54 -0700

    First Commit

---
+++ android/app/src/main/kotlin/com/example/finalquiz/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.finalquiz
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:finalquiz/startscreen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [
+                Color.fromARGB(255, 78, 13, 151),
+                Color.fromARGB(255, 107, 15, 168),
+              ],
+            ),
+          ),
+          child: StartScreen(),
+        ),
+      ),
+    ),
+  );
+}
