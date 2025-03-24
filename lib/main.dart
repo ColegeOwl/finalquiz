9bd99257093d3222427e23695a4683317026b341
Themistokli Dajko <themidajko02@gmail.com>
2025-03-24 19:46:23 -0400

    changes color of background

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,7 @@
+import 'package:finalquiz/quiz.dart';
 import 'package:flutter/material.dart';
-import 'package:finalquiz/startscreen.dart';
+//import 'package:finalquiz/startscreen.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-              colors: [
-                Color.fromARGB(255, 78, 13, 151),
-                Color.fromARGB(255, 107, 15, 168),
-              ],
-            ),
-          ),
-          child: StartScreen(),
-        ),
-      ),
-    ),
-  );
+  runApp((Quiz()));
 }
