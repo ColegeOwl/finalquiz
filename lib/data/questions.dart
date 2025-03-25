b046a4202b333e5f74dfda73cf63ed4efe1d5226
Themistokli Dajko <themidajko02@gmail.com>
2025-03-24 20:16:51 -0400

    create the questions based on football

---
+++ lib/data/questions.dart
@@ -0,0 +1,59 @@
+import 'package:finalquiz/models/quiz_questions.dart';
+
+const questions = [
+  QuizQuestions(
+    "Who is the highest goal score in soccer ",
+    [
+      'Christiano Ronaldo',
+      'Kevin de Bruyne',
+      'Lionel Messi',
+      'Pele'
+    ], 
+  ),
+   QuizQuestions(
+    "Who is not a Brazilian football player ",
+    [
+      'Robert Lewandowski',
+      'Ronaldo Nazário',
+      'Kaka',
+      'Pele'
+    ], 
+  ),
+   QuizQuestions(
+    "Which player changed to the most football clubs ",
+    [
+      'John Burridge',
+      'Zlatan Ibrahimovic',
+      'Giorgio Chiellini',
+      'David De Gea'
+    ], 
+  ),
+   QuizQuestions(
+    "Who won the 2004 European Cup ",
+    [
+      'Greece',
+      'Brazil',
+      'Spain',
+      'Germany'
+    ], 
+  ),
+   QuizQuestions(
+    "Who won the 2020 World Cup ",
+    [
+      'Argentina',
+      'Qatar',
+      'Japan',
+      'Spain'
+    ], 
+  ),
+   QuizQuestions(
+    "What team is part of the Premier league ",
+    [
+      'Liverpool',
+      'Olympiakos',
+      'Barcelona',
+      'Paris Saint-Germain'
+    ], 
+  ),
+
+];
